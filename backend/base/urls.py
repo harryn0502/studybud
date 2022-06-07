db5884b6f51f456e20b77c6f20eb6b8f30f4f491
Harryn Oh <ohharryn0502@gmail.com>
2022-06-07 18:18:55 +0100

    Added user registration

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path('login', views.loginPage, name='login'),
     path('logout', views.logoutPage, name='logout'),
+    path('register', views.registerPage, name='register'),
 
     path('', views.home, name='home'),
     path('room/<str:id>', views.room, name='room'),
