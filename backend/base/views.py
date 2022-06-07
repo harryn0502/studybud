21f7326dd956462eab2164a691561b669616a36e
Harryn Oh <ohharryn0502@gmail.com>
2022-06-07 16:56:15 +0100

    Added search functionality

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Room
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
+from django.db.models import Q
 
 # rooms = [
 #   {'id':1, 'name':"Let's learn python!"},
@@ -10,8 +13,15 @@ from .models import Room
 
 # Create your views here.
 def home(request):
-  rooms = Room.objects.all()
-  context = {'rooms':rooms}
+  q = request.GET.get('q') if request.GET.get('q') != None else ''
+  rooms = Room.objects.filter(
+    Q(topic__name__icontains=q) | 
+    Q(name__icontains=q) | 
+    Q(description__icontains=q)
+  )
+  topics = Topic.objects.all()
+  room_count = rooms.count()
+  context = {'rooms':rooms, 'topics':topics, 'room_count':room_count}
   return render(request, 'base/home.html', context)
 
 def room(request, id):
@@ -19,3 +29,34 @@ def room(request, id):
   context = {'room':room}
 
   return render(request, 'base/room.html', context)
+
+def createRoom(request):
+  form = RoomForm()
+  if request.method == 'POST':
+    form = RoomForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+
+  context = {'form':form}
+  return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+  room = Room.objects.get(id=pk)
+  form = RoomForm(instance=room)
+
+  if request.method == 'POST':
+    form = RoomForm(request.POST, instance=room)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+
+  context = {'form':form}
+  return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+  room = Room.objects.get(id=pk)
+  if request.method=='POST':
+    room.delete()
+    return redirect('home')
+  return render(request, 'base/delete.html', {'obj':room})
